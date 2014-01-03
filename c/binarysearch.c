a7b2fa823719af03b9738bfcaf426fa419ff4fe4
Denis Gatin <chizel@inbox.ru>
2014-01-03 17:13:15 +0200

    corrected mistakes

--- c/binarysearch.c
+++ c/binarysearch.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-signed int binarysearch(int *, uint32_t, uint32_t);
+signed int binarysearch(int *, uint32_t, int32_t);
 
 int main(void)
 {
-    uint32_t ans = 0;
+    int32_t position = 0;
     int arr[10] = {1, 3, 6, 7, 11, 13, 14, 16, 19, 22};
 
-    ans = binarysearch(arr, 10, 15);
-    printf("answer: %d\n", ans);
+    position = binarysearch(arr, 10, 16);
+    printf("answer: %d\n", position);
 
     return EXIT_SUCCESS;
 }
@@ -17,7 +18,7 @@ int main(void)
 /*
  *n0<n1<n2...
  */
-signed int binarysearch(int *arr, uint32_t arr_size, uint32_t search)
+signed int binarysearch(int *arr, uint32_t arr_size, int32_t search)
 {
     //array is empty
     if(!arr_size)
