307479175240198c5a389024b3ecb08fb6bf6f70
Denis Gatin <chizel@inbox.ru>
2014-01-07 19:07:25 +0200

    change variables` names

--- c/mergesort.c
+++ c/mergesort.c
@@ -26,34 +26,11 @@ int main(void)
 
     return EXIT_SUCCESS;
 }
-/*
-def mergesort(arr):
-    size = len(arr)
-
-    if size < 2:
-        return arr
-
-    middle = size / 2
-    arr1 = mergesort(arr[0:middle])
-    arr2 = mergesort(arr[middle:])
-    i = 0
-    arr_res = []
-
-    for item in arr1:
-        while(i < len(arr2)) and (arr2[i] < item):
-            arr_res.append(arr2[i])
-            i += 1
-        arr_res.append(item)
-
-    for j in range(i, len(arr2)):
-        arr_res.append(arr2[j])
-    return arr_res
-*/
 
 int32_t *mergesort(int32_t *arr, uint32_t size)
 {
-    uint32_t size_arr1, size_arr2, middle, i, j;
-    int32_t *arr1, *arr2, *k, *arr_res;
+    uint32_t size_sub_arr1, size_sub_arr2, middle, i, j;
+    int32_t *sub_arr1, *sub_arr2, *k, *arr_res;
 
     if(size < 2)
     {
@@ -61,32 +38,32 @@ int32_t *mergesort(int32_t *arr, uint32_t size)
     }
 
     middle = size / 2;
-    size_arr1 = middle;
-    size_arr2 = size - middle;
-    arr1 = mergesort(arr, size_arr1);
-    arr2 = mergesort(&arr[size_arr1], size_arr2);
-    arr_res = (int32_t  *)malloc(sizeof(int32_t) + (size_arr1 + size_arr2));
+    size_sub_arr1 = middle;
+    size_sub_arr2 = size - middle;
+    sub_arr1 = mergesort(arr, size_sub_arr1);
+    sub_arr2 = mergesort(&arr[size_sub_arr1], size_sub_arr2);
+    arr_res = (int32_t  *)malloc(sizeof(int32_t) + (size_sub_arr1 + size_sub_arr2));
 
     i = 0;
     j = 0;
     k = arr_res;
 
-    for(i = 0; i < size_arr1; i++)
+    for(i = 0; i < size_sub_arr1; i++)
     {
-        while( (j < size_arr2) && (arr1[i] > arr2[j]) )
+        while( (j < size_sub_arr2) && (sub_arr1[i] > sub_arr2[j]) )
         {
-            *k = arr2[j];
+            *k = sub_arr2[j];
             k++;
             j++;
         }
 
-        *k = arr1[i];
+        *k = sub_arr1[i];
         k++;
     }
 
-    while(j < size_arr2)
+    while(j < size_sub_arr2)
     {
-            *k = arr2[j];
+            *k = sub_arr2[j];
             k++;
             j++;
     }
