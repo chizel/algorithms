c4fdf47946b19975835b4da2163014986009e8b4
Denis Gatin <chizel@inbox.ru>
2014-01-09 21:05:58 +0200

    add test, clearing memory

--- c/mergesort.c
+++ c/mergesort.c
@@ -1,48 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include "testsort.h"
 
 int32_t *mergesort(int32_t *, uint32_t);
 
 int main(void)
 {
-    int i = 0;
-    int32_t arr[10]  = {9, 3, 5, 11, 7, 4, 12, 15, 14, 10};
-    int32_t *sorted_array;
+    int32_t *arr;
 
-    for(i = 0; i < 10; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    putc('\n', stdout);
-
-    sorted_array = mergesort(arr, 10);
-
-    for(i = 0; i < 10; i++)
-    {
-        printf("%d ", sorted_array[i]);
-    }
-    putc('\n', stdout);
+    arr = generate_array(ARR_SIZE);
+    print_array(arr, ARR_SIZE);
+    arr = mergesort(arr, ARR_SIZE);
+    check_sorting(arr, ARR_SIZE);
 
     return EXIT_SUCCESS;
 }
 
-int32_t *mergesort(int32_t *arr, uint32_t size)
+int32_t *mergesort(int32_t *arr, uint32_t arr_size)
 {
     uint32_t size_sub_arr1, size_sub_arr2, middle, i, j;
     int32_t *sub_arr1, *sub_arr2, *k, *arr_res;
 
-    if(size < 2)
+    if(arr_size < 2)
     {
         return arr;
     }
 
-    middle = size / 2;
+    middle = arr_size / 2;
     size_sub_arr1 = middle;
-    size_sub_arr2 = size - middle;
+    size_sub_arr2 = arr_size - middle;
     sub_arr1 = mergesort(arr, size_sub_arr1);
     sub_arr2 = mergesort(&arr[size_sub_arr1], size_sub_arr2);
-    arr_res = (int32_t  *)malloc(sizeof(int32_t) + (size_sub_arr1 + size_sub_arr2));
+    arr_res = malloc(sizeof(int32_t) * (size_sub_arr1 + size_sub_arr2));
+
+    if(arr_res == NULL)
+    {
+        printf("Can't allocate memory!\n");
+        exit(0);
+    }
 
     i = 0;
     j = 0;
@@ -67,10 +63,17 @@ int32_t *mergesort(int32_t *arr, uint32_t size)
             k++;
             j++;
     }
+    
+    if(size_sub_arr1 > 1)
+    {
+        free(sub_arr1);
+        sub_arr1 = NULL;
+    }
 
-    //не освобождается память!!!!
-    /*free(arr1);*/
-    /*free(arr2);*/
-
+    if(size_sub_arr2 > 1)
+    {
+        free(sub_arr2);
+        sub_arr2 = NULL;
+    }
     return arr_res;
 }
