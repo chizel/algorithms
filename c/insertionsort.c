87baaf9a3633f12bfadc856436ee4389abe97558
Denis Gatin <chizel@inbox.ru>
2014-01-09 19:13:17 +0200

    add sorting check

--- c/insertionsort.c
+++ c/insertionsort.c
@@ -1,43 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include "testsort.h"
 
 int32_t *insertionsort(int32_t *, uint32_t);
 
 int main(void)
 {
     int i = 0;
+    int32_t *arr;
 
-    int arr[10] = {5, 9, 3, 2, 1, 12, 7, 3, 10, 11};
-
-    for(i = 0; i < 10; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    putc('\n', stdout);
-
-    insertionsort(arr, 10);
-
-    for(i = 0; i < 10; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    putc('\n', stdout);
+    arr = generate_array(ARR_SIZE);
+    print_array(arr, ARR_SIZE);
+    insertionsort(arr, ARR_SIZE);
+    check_sorting(arr, ARR_SIZE);
 
     return EXIT_SUCCESS;
 }
 
-int32_t *insertionsort(int32_t *arr, uint32_t size)
+int32_t *insertionsort(int32_t *arr, uint32_t arr_size)
 {
     uint32_t i, j;
     int32_t tmp;
 
-    for(i = 1; i < size; i++)
+    for(i = 1; i < arr_size; i++)
     {
         j = i;
         tmp = arr[j];
 
-        while( j && (arr[j -1] > tmp) )
+        while( j && (arr[j - 1] > tmp) )
         {
             arr[j] = arr[j - 1];
             j--;
