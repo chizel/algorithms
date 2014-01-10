c2e436af3f55c5b14df2b2f398db86fa92eb9c59
Denis Gatin <chizel@inbox.ru>
2014-01-10 17:30:01 +0200

    doesn`t work

--- c/quicksort.c
+++ c/quicksort.c
@@ -1,40 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include "testsort.h"
 
-int8_t quicksort(int *, uint32_t, uint32_t);
+int32_t quicksort(int32_t *, uint32_t, uint32_t);
 
 int main(void)
 {
-    uint32_t i;
-    int arr[10] = {11, 10, 12, 13, 15, 14, 13, 16, 11, 12};
+    int32_t *arr;
 
-    quicksort(arr, 0, 9);
-
-    for(i = 0; i <= 9; i++)
-    {
-        printf("%d ", arr[i]);
-    } 
-
-    putchar('\n');
+    arr = generate_array(ARR_SIZE);
+    print_array(arr, ARR_SIZE);
+    quicksort(arr, 0, ARR_SIZE - 1);
+    check_sorting(arr, ARR_SIZE);
 
     return EXIT_SUCCESS;
 }
 
-int8_t quicksort(int *arr, uint32_t low, uint32_t high)
+int32_t quicksort(int32_t *arr, uint32_t low, uint32_t high)
 {
     //array have one or less elements, nothing to sort
-    if(high <= 1)
+    if( (high - low) < 2)
     {
         return -1;
     }
 
     uint32_t middle, i, j;
-    int32_t tmp;
+    int32_t tmp, point;
     middle = low + (high - low) / 2;
-    i = low;
+    i = low + 1;
     j = high;
+    point = arr[middle];
 
+    while(i <= j)
+    {
+        while( (arr[i] < point) && (i <= j) )
+        {
+            i++;
+
+            while( (arr[j] >= point) && (j > i) )
+            {
+                j--;
+            }
+        }
+
+
+        tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+
+        i++;
+        j--;
+    }
+            while( (arr[j] >= point) && (j > i) )
+            {
+                j--;
+            }
+printf("low: %d, high: %d, middle: %d\n", low, high, middle);
+    /*if(arr[j] < arr[low])*/
+    /*{*/
+        /*tmp = arr[j];*/
+        /*arr[j] = arr[low];*/
+        /*arr[low] = tmp;*/
+    /*}*/
+
+for(i = low; i <= high; i++)
+{
+    printf("%d ", arr[i]);
+}
+printf("\n=================================\n");
+/*if(middle==6)*/
+/*exit(0);*/
+        /*
     for(i = 0; i < middle; i++)
     {
         if(arr[i] > arr[middle])
@@ -49,6 +86,7 @@ int8_t quicksort(int *arr, uint32_t low, uint32_t high)
             arr[j] = tmp;
         }
     }
+    //*/
 
     if(low < middle)
     {
